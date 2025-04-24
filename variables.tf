7040d4fc80f9e88e935fa69b2e41ba1fbc470efe
teja <tejpaulsingadi@gmail.com>
2025-04-24 13:14:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "project_id" {
+  type        = string
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP Region"
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP Zone"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Name of the VM"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Machine type, e.g. e2-micro"
+}
+
+variable "image" {
+  type        = string
+  description = "OS image, e.g. debian-cloud/debian-11"
+}
+
+variable "network" {
+  type        = string
+  description = "Network to attach"
+}
+
+
