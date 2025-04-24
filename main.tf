6dd21d7c7a644a8e0d4db04624445297fec1416b
teja <tejpaulsingadi@gmail.com>
2025-04-24 13:13:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = var.network
+    access_config {}
+  }
+
+  tags = var.tags
+}
